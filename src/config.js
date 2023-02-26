06b35287357a44a8fbc4e86566d82f0ee1d279c6
JyotiKM29 <88364055+JyotiKM29@users.noreply.github.com>
2023-02-27 00:54:04 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteTitle: 'Jyoti Kumari | Fronted Developer ',
+  siteDescription:
+    'I am excited to use my skills as software engineer and student to bring your projects to life',
+  siteKeywords:
+    'Jyoti Kumari ,Jyoti KM , Jyoti, JyotiKM29, software developer, software engineer, content freelancer, Gym website, Town of book, web developer, react developer, outreachy, jalandhar, ikgptu, ptu, gwoc',
+  siteUrl: 'https://github.com/JyotiKM29',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Jyoti Kumari',
+  location: 'Jalandhar, India',
+  email: 'jyotikumari4442@gmail.com',
+  github: 'https://github.com/JyotiKM29',
+  twitterHandle: '',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/JyotiKM29',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/jyoti-km/',
+    },
+    {
+      name: 'Codepen',
+      url: 'https://codepen.io/Dev213',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/khatripriya4o7/',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/jyotis_tweet',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
